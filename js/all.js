aa3c710b0737a178f8fb4d243ad8a62cc6bd8535
fabianVeliz <velizfabianhoracio@gmail.com>
2014-07-25 17:44:20 -0300

    [js-language]

--- js/all.js
+++ js/all.js
@@ -1,1 +1,33 @@
-/* Code js */
+$(document).on('ready',_ready);
+
+var lang_es = {
+	'experience':'Experiencia',
+	'skills':'habilidades'
+}
+
+var lang_en = {}
+
+
+function _ready(){
+	$('.language ul li').on('click',changeLanguage);
+}
+
+
+function changeLanguage(){
+
+	var itemToChange = $.find("[data-world]");
+
+
+	/*itemToChange.each(function(){
+		lang_en[$(this).data('world')]='';
+	});
+	*/
+	console.log(itemToChange);
+
+	/*
+	$.each(lang_en, function(result,item) {
+		console.log(result,item);
+	});
+	*/
+	
+}
